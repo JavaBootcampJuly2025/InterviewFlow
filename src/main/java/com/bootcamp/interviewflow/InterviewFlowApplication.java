7f9a6753b2100a789509a08b0332ca0e1939d3c1
Alexander Biezbardis <mr.biezbardis@gmail.com>
2025-07-15 10:25:35 +0300

    Initial commit

---
+++ src/main/java/com/bootcamp/interviewflow/InterviewFlowApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.interviewflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bootcamp/interviewflow/InterviewFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.interviewflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
