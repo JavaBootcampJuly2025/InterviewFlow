bcd5af7303d8219131e67ec937d32c4b6f411d02
Karolis Grybas <Karolisgrybas@outlook.com>
2025-07-19 17:52:15 +0300

    implement user auth for application CRUD

--- src/main/java/com/bootcamp/interviewflow/repository/ApplicationRepository.java
+++ src/main/java/com/bootcamp/interviewflow/repository/ApplicationRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
 
     List<Application> findAllByUserId(Long userId);
+
+    Optional<Application> findByIdAndUserId(Long id, Long userId);
 }
