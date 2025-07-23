1634d5fbae9373c4d8a48598d509fc4762558a01
Alexander Biezbardis <mr.biezbardis@gmail.com>
2025-07-23 14:22:19 +0300

    Remove redundant classes

--- src/main/java/com/bootcamp/interviewflow/exception/ResumeNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.bootcamp.interviewflow.exception;
-
-public class ResumeNotFoundException extends RuntimeException {
-    public ResumeNotFoundException(String message) {
-        super(message);
-    }
-}
