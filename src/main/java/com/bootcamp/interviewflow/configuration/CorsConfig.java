5cb2feb3250c16e44d1572c89630f1c03f90b508
Alexander Biezbardis <mr.biezbardis@gmail.com>
2025-07-24 16:01:53 +0300

    Fix cors config

--- src/main/java/com/bootcamp/interviewflow/configuration/CorsConfig.java
+++ src/main/java/com/bootcamp/interviewflow/configuration/CorsConfig.java
@@ -1,37 +1,27 @@
 package com.bootcamp.interviewflow.configuration;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.List;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
+    private final String frontendDomain;
 
-    private final FrontendProperties frontendProperties;
-
-    public CorsConfig(FrontendProperties frontendProperties) {
-        this.frontendProperties = frontendProperties;
+    public CorsConfig(@Value("${interviewFlow.frontend.frontendDomain}") String frontendDomain) {
+        this.frontendDomain = frontendDomain;
     }
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        String origin = "http://" + frontendDomain;
 
-        String origin = "http://" + frontendProperties.getFrontendDomain() + ":" + frontendProperties.getFrontendPort();
-        configuration.setAllowedOriginPatterns(List.of(origin));
-
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
-        configuration.setMaxAge(3600L);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+        registry.addMapping("/**")
+                .allowedOrigins(origin)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
